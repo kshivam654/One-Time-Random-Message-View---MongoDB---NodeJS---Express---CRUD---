0aae1ba51911a3a27aa899bf7f800d835e808a25
Shivam Sharma <kshivam654@gmail.com>
2020-12-13 14:36:24 +0530

    fixed.

--- controller/message.js
+++ controller/message.js
@@ -4,17 +4,14 @@ exports.getMessage = async (req, res, next) => {
     Message.findOne()
         .then((product) => {
             console.log(product);
-            if (product != null) {
-                Message.deleteOne({ _id: product._id })
-                    .then((result) => {
-                        console.log("Message Been deleted");
-                        res.status(200).json(product);
-                    })
-                    .catch((error) => {
-                        console.log("Cant perfrom delete");
-                    });
-            }
-            res.status(201).json()
+            Message.deleteOne({ _id: product._id })
+                .then((result) => {
+                    console.log("Message Been deleted");
+                    res.status(200).json(product)
+                })
+                .catch((error) => {
+                    console.log("Cant perfrom delete");
+                });
         })
         .catch((error) => {
             console.error(error);
@@ -37,7 +34,7 @@ exports.postMessage = (req, res, next) => {
         .save()
         .then((result) => {
             console.log("Message Got ADDED to DB!!");
-            res.status(200).json();
+            res.status(200).json()
         })
         .catch((error) => {
             console.error("Error-> data insert");
